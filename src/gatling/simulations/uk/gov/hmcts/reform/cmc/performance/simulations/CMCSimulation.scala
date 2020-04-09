6f655e056d313719a8eff8f752967f14b4534d23
vijayvykuntam <vijay.vykuntam@hmcts.net>
2020-04-09 13:21:57 +0100

    added legal journey

--- src/gatling/simulations/uk/gov/hmcts/reform/cmc/performance/simulations/CMCSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/cmc/performance/simulations/CMCSimulation.scala
@@ -70,7 +70,7 @@ class CMCSimulation extends Simulation
          .maxDuration(7200)*/
        setUp(scenario_LC
          .inject(
-           rampUsers(50) during  (2700))
+           rampUsers(25) during  (2700))
          .protocols(legalhttpProtocol),
          /*scenario_NLC
            .inject(
